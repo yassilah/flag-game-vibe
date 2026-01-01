1906c56146cc4e75ec1e050f976984251c1f760a
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2026-01-01 12:06:07 +0100

    wip

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,18 @@
+<script setup>
+// If you want to use it in setup, import from the nuxtApp.
+const { $pwa } = useNuxtApp()
+
+const toast = useToast()
+
+onMounted(() => {
+   if ($pwa.offlineReady)
+      toast.success('App ready to work offline')
+})
+</script>
+
 <template>
    <UApp>
+      <NuxtPwaManifest />
       <UContainer class="min-h-screen px-4 py-4 sm:px-6 sm:py-8">
          <header class="mb-6 text-center sm:mb-8">
             <h1 class="text-primary mb-2 text-3xl font-bold sm:text-4xl md:text-5xl">
@@ -9,6 +22,19 @@
                Devinez le drapeau en 5 essais !
             </p>
          </header>
+         <div
+            v-show="$pwa.needRefresh"
+            class="mb-4 flex items-center justify-between rounded-lg bg-blue-100 p-4 text-blue-800 dark:bg-blue-900 dark:text-blue-300"
+         >
+            <span class="font-medium">
+               New content available, click on reload button to update.
+            </span>
+
+            <UButton @click="$pwa.updateServiceWorker()">
+               Reload
+            </UButton>
+         </div>
+
          <NuxtPage />
       </UContainer>
    </UApp>
